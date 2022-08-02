825227b1df88503940cc3671e1ab7d2dfcb6c9bd
Brian Napora <bnapora@gestaltdiagnostics.com>
2022-08-02 12:12:14 -0700

    changes to sql

---
+++ sql-queries/export-gclone_images.sql
@@ -0,0 +1,29 @@
+-- DECLARE create_date date := '2022-6-02';
+
+SELECT 
+"Slides"."imageGuid"
+,'/' || "Slides"."imageGuid" ||'/' || "Slides"."imageGuid" || '.svs'  as "src_image_path"
+,"Slides".id 
+,"Slides"."caseItemId"
+,accession
+,"Slides".label
+,"Slides".block
+,"Slides"."slideNumber"
+,"CDR_CaseSpecimens".label as case_specimen_label
+,"CDR_CaseSpecimens".description as case_specimen_description
+,date_trunc('day', "Slides"."createdAt")::date as "createdAt"
+-- -- ,'Pop_' || accession || '_' || "Slides".label || "Slides".block || '-' || "Slides"."slideNumber" ||'.svs' as new_slide_name
+-- ,'D:\PathologyV2\' || "imageGuid" ||'\' || "imageGuid" || '.svs' || ',Pop_' || accession || '\Pop_' || accession || '_' || "Slides".label || "Slides".block || '-' || "Slides"."slideNumber" ||'.svs' as "source, destination"
+
+FROM "CDR"."Slides"
+LEFT JOIN "public"."CDR_CaseSpecimens" ON "CDR_CaseSpecimens".id = "Slides"."caseSpecimenId"
+WHERE "accession" IN (select accession from "CDR"."Slides" 
+                        where "CDR"."Slides"."imageGuid" IS NOT NULL 
+                            AND "createdAt" >= '2022-6-02' 
+                        GROUP BY accession, "Slides".label || "Slides".block 
+                        HAVING count(accession) > 2 
+                            AND sum(CAST("Slides"."slideNumber" AS integer)) > 2 LIMIT 1000) 
+AND "CDR"."Slides"."createdAt" >= '2022-6-02' 
+AND "CDR"."Slides"."imageGuid" IS NOT NULL
+AND "CDR"."Slides"."slideNumber" IS NOT NULL
+ORDER BY "Slides"."accession", "Slides"."label", "Slides"."block", "Slides"."slideNumber" desc
--- sql-queries/groupby_imagecnt.sql
+++ sql-queries/groupby_imagecnt.sql
@@ -1,12 +1,12 @@
 select accession
+,"Slides".label || "Slides".block as block_label
 ,count(accession) AS count
+-- ,sum(CAST("Slides"."slideNumber" AS integer)) as sumcheck
 -- ,"slideIdentifier"
 -- ,block
 -- ,"imageGuid"
 -- ,"specimenIdentifier"
 ,"isImageAvailable"
-,"isImageArchived"
-,"isAssetAvailable"
 -- ,"createdAt"
 -- ,"updatedAt"
 -- ,"deletedAt"
@@ -16,10 +16,13 @@ select accession
 -- ,"caseSpecimenId"
 ,date_trunc('day', "createdAt")::date
 from "CDR"."Slides"
-where date_trunc('day', "createdAt")::date >= '2021-11-15'
-GROUP BY accession,"isImageAvailable"
-,"isImageArchived"
-,"isAssetAvailable"
+where date_trunc('day', "createdAt")::date >= '2022-6-02' 
+AND "CDR"."Slides"."imageGuid" IS NOT NULL
+AND "CDR"."Slides"."slideNumber" IS NOT NULL
+GROUP BY accession
+,"Slides".label || "Slides".block
+,"isImageAvailable"
 ,date_trunc('day', "createdAt")::date
-ORDER BY count desc
+HAVING count(accession) > 1 AND sum(CAST("Slides"."slideNumber" AS integer)) > 2
+ORDER BY accession desc
 LIMIT 1000
